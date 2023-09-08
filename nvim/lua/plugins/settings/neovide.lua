cd7a860ef77e31e3d8b62fae7821fc712997fb24
geza <denesgeza@gmail.com>
2023-09-08 19:56:52 +0300

    [neovide] updated install location

--- nvim/lua/plugins/settings/neovide.lua
+++ nvim/lua/plugins/settings/neovide.lua
@@ -99,11 +99,11 @@ vim.g.neovide_cursor_vfx_particle_phase = 1.5
 vim.g.neovide_cursor_vfx_particle_curl = 1.0
 -- }}}
 -- Keymaps {{{
-vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+vim.keymap.set("n", "<D-s>", ":w<CR>")      -- Save
+vim.keymap.set("v", "<D-c>", '"+y')         -- Copy
+vim.keymap.set("n", "<D-v>", '"+P')         -- Paste normal mode
+vim.keymap.set("v", "<D-v>", '"+P')         -- Paste visual mode
+vim.keymap.set("c", "<D-v>", "<C-R>+")      -- Paste command mode
 vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 
 vim.keymap.set("n", "<C-h>", "<C-w>h")
