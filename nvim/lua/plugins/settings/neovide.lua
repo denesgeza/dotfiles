46306e0206d6e689944a1f28daa322c2c2f5a5dc
geza <denesgeza@gmail.com>
2024-01-07 21:59:07 +0200

    neovide: font config
    - for version 0.12.1

--- nvim/lua/plugins/settings/neovide.lua
+++ nvim/lua/plugins/settings/neovide.lua
@@ -5,64 +5,53 @@ Customize = require("config.customize")
 vim.g.neovide_padding_top = 0
 vim.g.neovide_padding_bottom = 0
 vim.g.neovide_padding_right = 0
-vim.g.neovide_padding_left = 6
+vim.g.neovide_padding_left = 0
 
 -- Font settings {{{
--- vim.o.guifont = "Victor Mono:h14, Symbols Nerd Font:h13"
--- vim.o.guifont = "FiraCode NFM Retina:h12,Victor Mono Italic:h12:i, Symbols Nerd Font:h12"
-vim.o.guifont = "Maple Mono:h11, Symbols Nerd Font:h14"
-vim.opt.linespace = 3
-vim.g.neovide_scale_factor = 0.9
+vim.o.guifont = "Maple Mono,Symbols Nerd Font:h13"
+vim.opt.linespace = 2
+vim.g.neovide_scale_factor = 1.0
 
 -- Helper function for adjusting font size
 local change_scale_factor = function(delta)
   vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 end
 vim.keymap.set("n", "<C-=>", function()
-  change_scale_factor(1.25)
+  change_scale_factor(1.05)
 end)
 vim.keymap.set("n", "<C-->", function()
-  change_scale_factor(1 / 1.25)
+  change_scale_factor(1 / 1.05)
 end)
 -- }}}
---  Transparency settings {{{
-local alpha = function()
-  return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
-end
--- Add keybinds to change transparency
-local change_transparency = function(delta)
-  vim.g.neovide_transparency_point = vim.g.neovide_transparency_point + delta
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
-end
-vim.keymap.set({ "n", "v", "o" }, "<D-]>", function()
-  change_transparency(0.01)
-end)
-vim.keymap.set({ "n", "v", "o" }, "<D-[>", function()
-  change_transparency(-0.01)
-end)
 
--- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
 -- Transparent
 if Customize.transparency then
-  vim.g.neovide_transparency = 0.0
+  vim.g.neovide_transparency = 0.75
   vim.g.transparency = 0.8
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
-  -- Windows {{{
-  vim.g.neovide_window_floating_opacity = 0.8
-  -- Floating blur amount
-  vim.g.neovide_floating_blur_amount_x = 2.0
-  vim.g.neovide_floating_blur_amount_y = 2.0
-  -- }}}
 else
+  vim.g.neovide_transparency = 1.0
   vim.g.transparency = 1.0
-  vim.g.neovide_background_color = "#16161D"
 end
 -- }}}
+-- Floating blur {{{
+vim.g.neovide_window_floating_opacity = 0.2
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 1.0
+-- }}}
+-- Floating shadow {{{
+vim.g.neovide_floating_shadow = false
+vim.g.neovide_floating_z_height = 10
+vim.g.neovide_light_angle_degrees = 45
+vim.g.neovide_light_radius = 5
+-- }}}
 -- Animations {{{
-vim.g.neovide_scroll_animation_length = 0.3
+vim.g.neovide_scroll_animation_length = 0.5
 
 -- Scroll animation far lines
 vim.g.neovide_scroll_animation_far_lines = 1
+
+-- Underline automatic scaling
+vim.g.neovide_underline_stroke_scale = 2
 
 -- Mouse
 vim.g.neovide_hide_mouse_when_typing = true
@@ -73,7 +62,7 @@ vim.g.neovide_hide_mouse_when_typing = true
 --  Background theme
 --  auto in day time loads catppuccin latte instead of mocha
 -- Issues with the cursor in auto mode
-vim.g.neovide_theme = "dark" ---@type "auto" | "dark" | "light"
+vim.g.neovide_theme = "auto" ---@type "auto" | "dark" | "light"
 
 -- Refresh rate
 vim.g.neovide_refresh_rate = 60
@@ -84,30 +73,30 @@ vim.g.neovide_confirm_quit = false
 
 -- Fullscreen
 vim.g.neovide_fullscreen = true ---@type boolean
-vim.g.neovide_remember_window_size = true -- remember last window size from previous session
-
+-- remember last window size from previous session
+vim.g.neovide_remember_window_size = true ---@type boolean
 -- Profiler (shows a frametime graph in the upper left corner)
-vim.g.neovide_profiler = false
+vim.g.neovide_profiler = false ---@type boolean
 
 -- MacOs Alt is Meta
 vim.g.neovide_input_macos_alt_is_meta = true
 -- }}}
 -- Cursor settings {{{
-vim.g.neovide_cursor_animation_length = 0.23
-vim.g.neovide_cursor_trail_size = 0.4
+vim.g.neovide_cursor_animation_length = 0.4
+vim.g.neovide_cursor_trail_size = 0.5
 vim.g.neovide_cursor_antialiasing = true
 vim.g.neovide_cursor_animate_in_insert_mode = true
 vim.g.neovide_cursor_animate_command_line = true
 vim.g.neovide_cursor_unfocused_outline_width = 0.125
 
 -- Particle modes
-vim.g.neovide_cursor_vfx_mode = "railgun" ---@type "railgun" | "rain" | "pixiedust" | ""
-vim.g.neovide_cursor_vfx_opacity = 200.0
-vim.g.neovide_cursor_vfx_particle_lifetime = 2.0
-vim.g.neovide_cursor_vfx_particle_density = 7.0
-vim.g.neovide_cursor_vfx_particle_speed = 5.0
+vim.g.neovide_cursor_vfx_mode = "pixiedust" ---@type "railgun" | "rain" | "pixiedust" | ""
+vim.g.neovide_cursor_vfx_opacity = 300.0
+vim.g.neovide_cursor_vfx_particle_lifetime = 4.0
+vim.g.neovide_cursor_vfx_particle_density = 8.0
+vim.g.neovide_cursor_vfx_particle_speed = 4.0
 vim.g.neovide_cursor_vfx_particle_phase = 1.5
-vim.g.neovide_cursor_vfx_particle_curl = 1.0
+vim.g.neovide_cursor_vfx_particle_curl = 1.5
 -- }}}
 -- Keymaps {{{
 vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
