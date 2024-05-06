5f9f951415029ae79f2d81980c7997625e990dff
geza <denesgeza@gmail.com>
2024-05-06 14:33:11 +0300

    neovim: fix bufferline

--- neovim/lua/plugins/bufferline.lua
+++ neovim/lua/plugins/bufferline.lua
@@ -4,7 +4,9 @@ return {
   {
     "akinsho/bufferline.nvim",
     enabled = Is_enabled("bufferline"),
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
+    lazy = false,
     keys = {
       -- { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
       -- { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
@@ -18,6 +20,31 @@ return {
     },
     opts = {
       options = {
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        -- stylua: ignore
+        close_command = function(n) require("mini.bufremove").delete(n, false) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+        sort_by = "relative_directory", ---@type 'insert_after_current' |'insert_at_end' | 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
+        show_tab_indicators = true, ---@type boolean
+        show_duplicate_prefix = true, ---@type boolean, -- whether to show duplicate buffer prefix
+        duplicates_across_groups = true, -- whether to consider duplicate paths in different groups as duplicates
+        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+        move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
+        -- can also be a table containing 2 custom separators
+        -- [focused and unfocused]. eg: { '|', '|' }
+        separator_style = "thick", ---@type "slant" | "slope" | "thick" | "thin" | { "any", "any" },
+        enforce_regular_tabs = true,
+        auto_toggle_bufferline = true, ---@type boolean
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("core.icons").diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
         offsets = {
           {
             filetype = "neo-tree",
@@ -28,5 +55,16 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd("BufAdd", {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
   },
 }
