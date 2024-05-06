5fe7d5e9cee5ee26627d972caae99f19242fd954
geza <denesgeza@gmail.com>
2024-05-06 14:33:41 +0300

    neovim: added mini-files, dadbod, visual-multi

--- neovim/lua/plugins/init.lua
+++ neovim/lua/plugins/init.lua
@@ -1,3 +1,8 @@
 return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  {
+    "sammce/fleeting.nvim",
+    lazy = false,
+    -- Commands :Fleeting, :FleetingReset, :FleetingDelete
+  },
 }
