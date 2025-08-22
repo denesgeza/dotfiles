722e5a1c8fe90ecff340c67e803ed049bb1ff7dd
geza.denes <denesgeza@gmail.com>
2025-08-22 18:59:56 +0300

    gvim: update blink setup

--- gvim/lua/pack/blink.lua
+++ gvim/lua/pack/blink.lua
@@ -4,7 +4,7 @@ local borders = 'single' ---@type 'single' | 'double' | 'padded' | 'solid' | 'sh
 vim.pack.add {
   { src = 'https://github.com/L3MON4D3/LuaSnip' },
   { src = 'https://github.com/rafamadriz/friendly-snippets' },
-  { src = 'https://github.com/saghen/blink.cmp', version = 'v1.6.0' },
+  { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range '*' },
 }
 
 require('luasnip').setup {}
